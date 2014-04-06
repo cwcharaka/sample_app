a4476b16ef8c39af8aa643a330697465c2797d13
charaka <cwcharaka@gmail.com>
2014-04-06 19:33:45 +1000

    Chapter 6 question Compete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,6 +52,17 @@ describe User do
 
     it { should_not be_valid }
   end
+
+  describe "email address with mix case" do
+  	let(:mixed_case_email) { "Foo@ExaMPLE.CoM" }
+  	
+  	it "should be saved as all lower-case" do
+  		@user.email = mixed_case_email
+  		@user.save
+  		expect(@user.reload.email).to eq mixed_case_email.downcase
+  		
+  	end
+  end
   
   describe "when passwod is not present" do
   	before do
