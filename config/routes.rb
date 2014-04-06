508e2b2f8dafec19fd9ab9b4eef2142b50126475
charaka <cwcharaka@gmail.com>
2014-04-06 13:53:19 +1000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SampleApp::Application.routes.draw do
  match '/help',    to: 'static_pages#help',    via: 'get'
  match '/about',   to: 'static_pages#about',   via: 'get'
  match '/contact', to: 'static_pages#contact', via: 'get'
+ match '/signup',  to: 'users#new',            via: 'get'
  #get "static_pages/home"
  #get "static_pages/help"
  #get "static_pages/about"
